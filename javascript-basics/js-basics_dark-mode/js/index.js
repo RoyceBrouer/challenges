93cfef83f193657b7135ff2a15399bf7210760f2
Royce Brouër <royce@systemli.org>
2023-08-17 16:19:08 +0200

    js-basics_dark-mode

--- javascript-basics/js-basics_dark-mode/js/index.js
+++ javascript-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
 const bodyElement = document.querySelector('[data-js="body"]');
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
