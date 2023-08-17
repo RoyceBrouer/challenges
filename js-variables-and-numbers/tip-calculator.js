b5a41091effb91125600c9a74897f085911f03da
Royce Brouër <royce@systemli.org>
2023-08-17 11:27:10 +0200

    challenge on tip-calculator

---
+++ js-variables-and-numbers/tip-calculator.js
@@ -0,0 +1,7 @@
+const mealCost = 6.5;
+const tipPercentage = 0.15;
+
+let tipAmount = mealCost * tipPercentage;
+let totalCost = mealCost + tipAmount;
+
+console.log("The total cost of your bill is " + totalCost + " euros");
