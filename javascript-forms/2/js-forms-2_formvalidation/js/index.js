b62b29ff95a82ffe2e9f6a7850fede7678bb4cfe
Royce Brouër <royce@systemli.org>
2023-08-24 10:58:18 +0200

    finished

--- javascript-forms/2/js-forms-2_formvalidation/js/index.js
+++ javascript-forms/2/js-forms-2_formvalidation/js/index.js
@@ -10,3 +10,26 @@ form.addEventListener("submit", (event) => {
 
   console.log(data);
 });
+
+// const form = document.querySelector('[data-js="form"]');
+// const termsAndCoditions = document.querySelector(#"terms-of-service");
+
+// form.addEventListener("submit", (event) {
+//   event.preventDefault();
+
+// })
+/* Make sure that the user submits all 
+relevant information in a correct format.
+
+Use the following guidelines:
+
+- First and last name are required and are 
+allowed to have at least 4 and up to 30 characters.
+- The user needs to be at least 14 years old.
+- We need a correct email adress (i.e. including 
+  an `@` symbol) for contacting the user.
+- It's necessary that the user has read the terms 
+of service and agrees to them.
+
+Switch to the [`index.html`](./index.html) file 
+and validate the form in the mentioned way! */
