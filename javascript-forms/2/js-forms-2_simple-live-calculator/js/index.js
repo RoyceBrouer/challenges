741c1a5a3b4b57a3fb4f25e1e3ac21811ae43188
Royce Brouër <royce@systemli.org>
2023-08-24 11:30:49 +0200

    still Nan when empty string

--- javascript-forms/2/js-forms-2_simple-live-calculator/js/index.js
+++ javascript-forms/2/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,26 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+console.log("before: ", firstInput.value);
+
+firstInput.addEventListener("input", () => {
+  result.textContent = `Result: ${
+    parseInt(firstInput.value) * parseInt(secondInput.value)
+  }`;
+  console.log("after: ", firstInput.value);
+  // secondInput.value = ++firstInput * +secondInput;
+});
+
+secondInput.addEventListener("input", () => {
+  result.textContent = `Result: ${
+    parseInt(firstInput.value) * parseInt(secondInput.value)
+  }`;
+  // firstInput.value = +firstInput * +secondInput;
+  // secondInput.value = ++firstInput * +secondInput;
+});
+
+/* There are two form fields where the user can enter a value.
+As soon as one of these values change, both values shall be 
+multiplied. The result is to be rendered with the help of 
+the output element. */
