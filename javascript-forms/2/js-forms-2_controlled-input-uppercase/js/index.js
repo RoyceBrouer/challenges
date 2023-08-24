012adf2a3dd4e09452468f1d253dac27170848bf
Royce Brouër <royce@systemli.org>
2023-08-24 11:07:52 +0200

    finished

--- javascript-forms/2/js-forms-2_controlled-input-uppercase/js/index.js
+++ javascript-forms/2/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,15 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", () => {
+  input.value = input.value.toUpperCase();
+});
+
+/* Please consider
+
+- the `input` event as well as
+- how to read and write the value of a form field. 
+There's definitely a difference between the value of 
+the event's target and the (after-event) value of your 
+input field.*/
