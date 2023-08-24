3be66754b928945fabba57c3374311a932659302
Royce Brouër <royce@systemli.org>
2023-08-24 16:42:56 +0200

    still question of NaN will leave it like this to learn

--- javascript-forms/2/js-forms-2_simple-live-calculator/js/index.js
+++ javascript-forms/2/js-forms-2_simple-live-calculator/js/index.js
@@ -7,20 +7,19 @@ const result = document.querySelector('[data-js="result"]');
 console.log("before: ", firstInput.value);
 
 firstInput.addEventListener("input", () => {
-  result.textContent = `Result: ${
+  result.textContent = `${
     parseInt(firstInput.value) * parseInt(secondInput.value)
-  }`;
+  }`; //this is not necessary if you multiply it should be converted to a number already
   console.log("after: ", firstInput.value);
-  // secondInput.value = ++firstInput * +secondInput;
 });
 
 secondInput.addEventListener("input", () => {
-  result.textContent = `Result: ${
+  result.textContent = `${
     parseInt(firstInput.value) * parseInt(secondInput.value)
   }`;
-  // firstInput.value = +firstInput * +secondInput;
-  // secondInput.value = ++firstInput * +secondInput;
 });
+//different solution is to update the result...
+//Why it is NaN is not solved for me
 
 /* There are two form fields where the user can enter a value.
 As soon as one of these values change, both values shall be 
