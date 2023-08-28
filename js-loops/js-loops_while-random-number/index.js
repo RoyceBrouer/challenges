eae6f093ea8586e076b9d14d36931d6c0d3bac26
Royce Brouër <royce@systemli.org>
2023-08-28 16:24:53 +0200

    initial commit is it correct?

--- js-loops/js-loops_while-random-number/index.js
+++ js-loops/js-loops_while-random-number/index.js
@@ -6,9 +6,23 @@ let number = 0;
 let count = 0;
 
 // --v-- write/change code here --v--
-
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random();
+}
 // --^-- write/change code here --^--
 
 console.log(
   `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
 );
+/* Create a `while` loop that
+
+- runs if `number` is less or equal to 0.9,
+- increments `count` by 1 in each iteration
+- logs `number` to the console
+- sets `number` to a random number; you can 
+use `Math.random()` here.
+
+If you're done, test your loop by refreshing 
+the browser several times and watching the console!*/
