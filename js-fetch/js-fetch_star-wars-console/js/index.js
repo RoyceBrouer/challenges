82d94337713aa6b8d1c876dc3d278f1d83e3b34f
Royce Brouër <royce@systemli.org>
2023-09-04 11:41:27 +0200

    initial commit

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,21 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch("https://swapi.dev/api/people");
+
+    if (!response.ok) {
+      console.log("no response");
+    } else {
+      const jsDataSwapi = await response.json();
+      console.log(jsDataSwapi);
+      const eyeColorR2D2 = jsDataSwapi.results[2].eye_color;
+      console.log(eyeColorR2D2);
+    }
+  } catch (err) {
+    console.log("oopsie ", err.message);
+  }
+}
 
 fetchData();
