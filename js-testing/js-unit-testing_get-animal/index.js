a85d2e7cf44866adf287d64404294d80ada46a2a
Royce Brouër <royce@systemli.org>
2023-09-05 11:05:34 +0200

    finished

--- js-testing/js-unit-testing_get-animal/index.js
+++ js-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return "I totally love cats!";
+  } else if (!animal) {
+    return "I do not like animals at all!";
+  } else if (typeof animal === "string" && animal !== "cats") {
+    return `I like ${animal}!`;
+  }
+}
