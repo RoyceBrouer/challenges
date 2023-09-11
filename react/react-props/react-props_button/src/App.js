9298d40d76ae9c83c4343fc4e57298b1a1937e57
Royce Brouër <royce@systemli.org>
2023-09-11 16:52:52 +0200

    initial commit

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button color="green" disabled={false} text="best button" />;
+}
+
+function Button({ color, disabled, text }) {
+  return (
+    <button style={{ backgroundColor: { color } }} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
