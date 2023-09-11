e7a3400d27a39678f49cd7e0d547be995d78dba2
Royce Brouër <royce@systemli.org>
2023-09-11 16:02:43 +0200

    initial commit

--- react/react-props/react-props_sum/src/App.js
+++ react/react-props/react-props_sum/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA="3" valueB="5" />;
+}
+
+function Sum({ valueA, valueB }) {
+  let result = Number(valueA) + Number(valueB);
+  return (
+    <h1>
+      {valueA} + {valueB} = {result}
+    </h1>
+  );
 }
