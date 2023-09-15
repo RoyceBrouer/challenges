6c0853ba986c9b8e43597a5260f1b53b66823d9a
root <root@DESKTOP-AGF6IAJ.localdomain>
2023-09-15 12:29:05 +0200

    initial commit journal first

--- react/react-3/react-state-3_journal-app-form/src/components/EntryForm/index.js
+++ react/react-3/react-state-3_journal-app-form/src/components/EntryForm/index.js
@@ -1,9 +1,19 @@
 import "./EntryForm.css";
 import Button from "../Button";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newEntry = Object.fromEntries(formData);
+    //const data = Object.fromEntries(formData);
+    //console.log(data);
+    //const newEntry = {motto:data.motto,notes: date.notes}
+    onAddEntry(newEntry);
+    event.target.reset();
+  };
   return (
-    <form className="entry-form">
+    <form className="entry-form" onSubmit={handleSubmit}>
       <h2 className="entry-form__title">New Entry</h2>
       <div className="entry-form__fields">
         <div className="entry-form__field">
