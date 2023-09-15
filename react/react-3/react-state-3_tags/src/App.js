675153199298d9b31b9c5c5c4024c02cf43ec2ce
root <root@DESKTOP-AGF6IAJ.localdomain>
2023-09-15 11:35:57 +0200

    initial attempt

--- react/react-3/react-state-3_tags/src/App.js
+++ react/react-3/react-state-3_tags/src/App.js
@@ -6,10 +6,18 @@ import List from "./components/List/index.js";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  const handleAddTag = (newTag) => {
+    setTags([...tags, newTag]);
+  };
+
+  const handleDeleteTag = (tagToDelete) => {
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+  };
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List onDeleteTag={handleDeleteTag} tags={tags} />
     </main>
   );
 }
