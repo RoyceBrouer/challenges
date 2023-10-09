e9397e27ed37210b9501710713a842afcb4cd5df
Royce Brouër <royce@systemli.org>
2023-10-09 15:34:31 +0200

    not finished

--- backend-read/backend-read_products/db/models/Product.js
+++ backend-read/backend-read_products/db/models/Product.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const jokeSchema = new Schema({
-  product: { type: String, required: true },
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  currency: { type: String, required: true },
 });
 
-const Product = mongoose.models.Joke || mongoose.model("Joke", jokeSchema);
+const Product =
+  mongoose.models.Product || mongoose.model("Product", productSchema);
 
-export default Joke;
+export default Product;
